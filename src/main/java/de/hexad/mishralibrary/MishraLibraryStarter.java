ad1041370a4a514052b6e00c6aab9ecdac735de9
unknown <abhishekamishra7@gmail.com>
2020-10-30 17:51:44 +0530

    project structure added.

---
+++ src/main/java/de/hexad/mishralibrary/MishraLibraryStarter.java
@@ -0,0 +1,14 @@
+package de.hexad.mishralibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MishraLibraryStarter {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MishraLibraryStarter.class, args);
+	}
+
+}
